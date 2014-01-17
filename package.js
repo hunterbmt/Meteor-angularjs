ec8354d9e66652e72a90f611071c626cdf0c3bdd
HunterBMT <hunterbmt@gmail.com>
2014-01-17 14:17:11 +0700

    add momment.js

--- package.js
+++ package.js
@@ -11,6 +11,8 @@ Package.on_use(function (api) {
 
   api.add_files('angular.js', 'client');
   api.add_files('angular-route.js', 'client');
+  api.add_files('moment.min.js', 'client');
+  api.add_files('angular-moment.min.js', 'client');
   api.add_files('client.js', 'client');
  
   api.add_files('server.js', 'server');
